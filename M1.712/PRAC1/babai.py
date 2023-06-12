c52fdcf53d2847d6ad5193d56cd8290fcebf78b7
Roger Calaf Planell <rogercalaf@gmail.com>
2023-06-12 18:53:07 +0200

    without transpose

--- M1.712/PRAC1/babai.py
+++ M1.712/PRAC1/babai.py
@@ -15,11 +15,11 @@ def ortogonal_flaw(B):
 
 def babai(base, vector):
     #Pas1. Escrivim w com a suma de producte de vectors i obtenim els coeficients
-    x = np.linalg.solve(base.T, vector)
+    x = np.linalg.solve(base, vector)
     #Pas2. Arrodonim els coeficients obtinguts a l'enter mes proper
     rounded = np.round(x, 0)
     #Pas3. Obtenim el vector resultant aplicant els coeficients x per cada coordenada.
-    v = np.dot(base.T, rounded)
+    v = np.dot(base, rounded).astype(int)
     print (f"Closest vector: {v}")
     return v
 
--- M1.712/PRAC1/ggh.py
+++ M1.712/PRAC1/ggh.py
@@ -2,14 +2,25 @@ import numpy as np
 import random
 import sys
 
-def ggh():
-    pass
+def decrypt(c, privateKey, pubKey):
+    x = np.dot(np.linalg.inv(privateKey), c)
+    rounded = np.round(x, 0)
+    v = np.dot(privateKey, rounded)
+    Winv = np.linalg.inv(pubKey)
+    dec = np.dot(v, Winv)
+    m = dec.astype(int)
+    return m
+
+def encrypt(m, pubKey):
+    r = np.random.randint(-5,5, size=(1,len(pubKey)))
+    cyphertext = np.dot(pubKey, m) + r
+    return cyphertext[0]
 
 def rand_unimod(n):
     random_matrix = [ [np.random.randint(-10,10,) for _ in range(n) ] for _ in range(n) ]
     upperTri = np.triu(random_matrix,0)
     lowerTri = [[np.random.randint(-10,10) if x<y else 0 for x in range(n)] for y in range(n)]  
-
+    #print(random_matrix, upperTri, lowerTri)
     #we want to create an upper and lower triangular matrix with +/- 1 in the diag  
     for r in range(len(upperTri)):
         for c in range(len(upperTri)):
@@ -20,7 +31,7 @@ def rand_unimod(n):
                 else:
                     upperTri[r][c]=-1
                     lowerTri[r][c]=-1
-				
+    #print(random_matrix, upperTri, lowerTri)	
     uniModular = np.matmul(upperTri,lowerTri)
     return uniModular
 
@@ -28,10 +39,17 @@ def main():
     print("GGH's algorithm (CVP)")
     print(f"Loading input values...")
     base = np.loadtxt(sys.argv[1], dtype='i', delimiter=' ')
+    m = np.loadtxt(sys.argv[2], dtype='i', delimiter=' ')
     u = rand_unimod(len(base))
+    #print(f"U: {u}")
     print(f"det(u) = {np.round(np.linalg.det(u))}")
     w = np.matmul(base,u)
     print(f"B: {base}\nW: {w}\n")
+    print(f"m: {m}")
+    c = encrypt(m, w)
+    print(f"Encrypt ({m}) -> ({c})")
+    d = decrypt(c, base, w.T)
+    print(f"Decrypt ({c}) -> ({d})")
 
 if __name__ == "__main__":
     main()
