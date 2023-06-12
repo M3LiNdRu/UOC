fdff00eaba3e4c89b91abd1d8df7f93ec4a2aeb5
Roger Calaf Planell <rogercalaf@gmail.com>
2023-06-12 22:52:51 +0200

    ggh changes

--- M1.712/PRAC1/ggh.py
+++ M1.712/PRAC1/ggh.py
@@ -1,10 +1,9 @@
 import numpy as np
-import random
 import sys
 
 def decrypt(c, privateKey, pubKey):
     x = np.dot(np.linalg.inv(privateKey), c)
-    rounded = np.round(x, 0)
+    rounded = np.round(x, 0).astype(int)
     v = np.dot(privateKey, rounded)
     Winv = np.linalg.inv(pubKey)
     dec = np.dot(v, Winv)
@@ -12,38 +11,32 @@ def decrypt(c, privateKey, pubKey):
     return m
 
 def encrypt(m, pubKey):
-    r = np.random.randint(-5,5, size=(1,len(pubKey)))
-    cyphertext = np.dot(pubKey, m) + r
+    # r = np.array([[-1,1,1,-1]])
+    r = np.random.randint(-1,1, size=(1,len(pubKey))) 
+    cyphertext = np.dot(pubKey,m) + r
     return cyphertext[0]
 
 def rand_unimod(n):
-    random_matrix = [ [np.random.randint(-10,10,) for _ in range(n) ] for _ in range(n) ]
-    upperTri = np.triu(random_matrix,0)
-    lowerTri = [[np.random.randint(-10,10) if x<y else 0 for x in range(n)] for y in range(n)]  
-    #print(random_matrix, upperTri, lowerTri)
-    #we want to create an upper and lower triangular matrix with +/- 1 in the diag  
-    for r in range(len(upperTri)):
-        for c in range(len(upperTri)):
-            if(r==c):
-                if bool(random.getrandbits(1)):
-                    upperTri[r][c]=1
-                    lowerTri[r][c]=1
-                else:
-                    upperTri[r][c]=-1
-                    lowerTri[r][c]=-1
-    #print(random_matrix, upperTri, lowerTri)	
-    uniModular = np.matmul(upperTri,lowerTri)
-    return uniModular
+    l = np.tril(np.random.randint(-10, 10, size=(n,n))).astype(int)
+    u = np.triu(np.random.randint(-10, 10, size=(n,n))).astype(int)
+    for i in range(0, n):
+        l[i, i] = u[i, i] = 1
+    return np.dot(l,u)
+
+def key_gen(base):
+    u = rand_unimod(len(base))
+    #print(f"U: {u}")
+    print(f"det(u) = {np.round(np.linalg.det(u))}")
+    return np.matmul(base,u)
+    
 
 def main():
     print("GGH's algorithm (CVP)")
     print(f"Loading input values...")
     base = np.loadtxt(sys.argv[1], dtype='i', delimiter=' ')
     m = np.loadtxt(sys.argv[2], dtype='i', delimiter=' ')
-    u = rand_unimod(len(base))
-    #print(f"U: {u}")
-    print(f"det(u) = {np.round(np.linalg.det(u))}")
-    w = np.matmul(base,u)
+    #w = key_gen(base)
+    w = np.loadtxt(sys.argv[3], dtype='i', delimiter=' ')
     print(f"B: {base}\nW: {w}\n")
     print(f"m: {m}")
     c = encrypt(m, w)
